2963a790a64a05c2c981317bb02d39013f1b9bc8
Shikhar Mohan <shikharmohan2016@u.northwestern.edu>
2014-09-05 20:21:14 -0700

    set up login controller, objects

---
+++ MatchedUp/SMLoginViewController.m
@@ -0,0 +1,57 @@
+//
+//  SMLoginViewController.m
+//  MatchedUp
+//
+//  Created by Shikhar Mohan on 9/5/14.
+//  Copyright (c) 2014 Shikhar Mohan. All rights reserved.
+//
+
+#import "SMLoginViewController.h"
+
+@interface SMLoginViewController ()
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
+
+@end
+
+@implementation SMLoginViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+#pragma mark - IBActions
+- (IBAction)loginButtonPressed:(UIButton *)sender
+{
+    
+}
+
+
+@end
