282a6c743e1d2929d4be30382bd753f1918652ae
Shikhar Mohan <shikharmohan2016@u.northwestern.edu>
2014-09-10 00:12:44 -0700

    added leopard pic

---
+++ MatchedUp/SMTestUser.m
@@ -0,0 +1,13 @@
+//
+//  SMTestUser.m
+//  MatchedUp
+//
+//  Created by Shikhar Mohan on 9/10/14.
+//  Copyright (c) 2014 Shikhar Mohan. All rights reserved.
+//
+
+#import "SMTestUser.h"
+
+@implementation SMTestUser
+
+@end
